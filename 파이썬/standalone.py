2bd92940cb87a36c3c6d65152be91e342a75f0b7
Eugene Jun <genenamu@gmail.com>
2023-03-06 16:43:40 +0900

    Make: stadalon.py

---
+++ 파이썬/standalone.py
@@ -0,0 +1,7 @@
+"""스텐드얼로운(standalone) 프로그램
+지금까지는 파이썬의 대화식 인터페이스에서 코드를 작성하고 실행했다.
+이번에 만들 파이썬 프로램은 <<< 가 존재하지 않는다.
+실행하기위해서는 터미널 창에서 다음과 같이 입력하면 된다.
+$ python standalon.py"""
+
+print("this standalon program works!")
