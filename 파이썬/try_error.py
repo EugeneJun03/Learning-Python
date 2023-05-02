bcd35a26476ea6de12ff886019bbf46a800154f6
Eugene Jun <genenamu@gmail.com>
2023-05-03 00:33:19 +0900

    feat: add else:
    else can be used in try-exception statement

--- 파이썬/try_error.py
+++ 파이썬/try_error.py
@@ -13,3 +13,11 @@ x is 13
 What's x? cat
 x is not a integer
 """
+
+#else can be used in try-exception statement
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not a integer")
+else:
+    print(f'x is {x}')
